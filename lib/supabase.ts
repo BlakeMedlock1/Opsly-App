6ea4489e68eba49c9b5841695099839d3c8ac611
BlakeMedlock1 <m044803m@student.staffs.ac.uk>
2025-06-02 11:02:01 +0100

    added supabase to application

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://bgpgijfctokperesyyvd.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJncGdpamZjdG9rcGVyZXN5eXZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4MDM2NDIsImV4cCI6MjA2NDM3OTY0Mn0.MObMgxBgEacb2GWzObtfZ9RDDzxmVv_YQzO5ZyOJ_WQ';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
